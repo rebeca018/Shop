c4f8cc16607b7fb5fe05de056ff1bc79a9c35fc6
rebeca018 <rebecasnascimento018@gmail.com>
2024-05-17 13:11:17 -0400

    Commit inicial

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "fakestoreapi.com"
+        ]
+    }
+};
+
+export default nextConfig;
---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+
+
+export const metadata = {
+  title: "Meu Shop.com",
+  description: "E-commerce",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
