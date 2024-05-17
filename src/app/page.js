3679987e7e2e78957cc512e629802936785f77d2
rebeca018 <rebecasnascimento018@gmail.com>
2024-05-17 14:23:49 -0400

    Commit aula 17/05/2024

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default async function Home() {
+    const response = await fetch("https://fakestoreapi.com/products/");
+    const data = await response.json();
 
   return (
-    <main>
+    <main className={styles.main}>
+      {data.map((products)=> 
+        <div className={styles.card} key={products.id}>
+            <p>{products.title}</p>
+            <Image 
+              width={200} 
+              height={200} 
+              src={products.image} 
+            />
+
+        </div>
+      )}
     </main>
   );
 }
