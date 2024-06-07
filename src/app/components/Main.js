bf1d68e80773af377f26b23165537ffc46624b37
Rebeca <rebecasnascimento018@gmail.com>
2024-06-07 14:59:56 -0400

    Commit aula 07/06

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,13 +1,62 @@
+'use client'
 import Image from "next/image";
 import styles from "./main.module.css";
+import { useEffect, useState } from "react";
 
-export default async function Main() {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
+export default function Main() {
+
+    const [listProduct, setListProduct] = useState([]);
+
+    useEffect(() => {
+      const getProduct = async () => {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+
+        setListProduct(data);
+      }
+        getProduct();
+    }, [] ); 
+
+    const orderAz = () => {
+      const newList = [...listProduct].sort( (a,b) => 
+        a.title.localeCompare(b.title)
+      );
+      setListProduct(newList);
+    }
+
+    const orderZa = () => {
+      let newList = [...listProduct].sort( (a,b) => 
+        a.title.localeCompare(b.title)
+      );
+      newList = newList.reverse();
+      setListProduct(newList);
+    }
+    
+    const precoMe = () => {
+      let newList = [...listProduct].sort( (a,b) => 
+        a.price - b.price
+      );
+      setListProduct(newList);
+    }
+
+    const precoMa = () => {
+      let newList = [...listProduct].sort( (a,b) => 
+        a.price - b.price
+      );
+      newList = newList.reverse();
+      setListProduct(newList);
+    }
 
   return (
+    <>
+    <div>
+      <button onClick={orderAz}>A-Z</button>
+      <button onClick={orderZa}>Z-A</button>
+      <button onClick={precoMa}>Preço: maior - menor</button>
+      <button onClick={precoMe}>Preço: menor - maior</button>
+    </div>
     <main className={styles.main}>
-      {data.map((products)=> 
+      {listProduct.map((products)=> 
           <div className={styles.card}>
             <Image
               width={220}
@@ -24,5 +73,6 @@ export default async function Main() {
           </div>
       )}
     </main>
+    </>
   );
 }
